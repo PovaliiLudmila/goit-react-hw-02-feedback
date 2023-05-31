e43202d5b2a973793f9ecea160eeb15b253317e9
PovaliiLudmila <118770513+PovaliiLudmila@users.noreply.github.com>
2023-06-01 00:35:45 +0300

    1

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 import css from './Notification.module.css';
 
-export const Notification = ({message}) => (
-    <p className={css.notification_text}>
+export const Notification = ({message}) => {
+    
+    return <p className={css.notification_text}>
         {message}
     </p>
-)
+    };
 
 Notification.propTypes = {
     message: PropTypes.string.isRequired
